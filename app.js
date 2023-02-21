3ca43944e3f6cc7d27ae6c3d632e535abb96e1aa
Pr454th <prasathkarthiban1999@gmail.com>
2023-02-21 17:56:56 +0530

    Set and Get note done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const router=require('./routes/routes')
 const cors=require('cors');
 
 app.use(cors());
+app.use(express.json());
 app.use('/',router);
 const port=4000;
 app.listen(port,()=>{
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const {setNote, getNote}=require('../controllers/requestHandler');
 
-router.post('/setnote/:id',setNote);
+router.post('/setnote',setNote);
 router.get('/getnote/:id',getNote);
 
 module.exports=router;
