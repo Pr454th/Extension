9a3a7e65ebda418d3cf292c9bda7e1ec276503fe
Pragadesh BS <pragadeshbs@protonmail.com>
2023-02-27 16:48:08 +0530

    add support for password protection

--- db/noteModel.js
+++ db/noteModel.js
@@ -1,25 +1,29 @@
-const mongoose=require('mongoose');
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("connection successfull...");
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("connection successfull...");
 });
 
-const schema=mongoose.Schema({
-    id:{
-        type:String
-    },
-    note:{
-        type:String,
-        default:"Empty"
-    },
-    createdAt:{
-        type:Date,
-        expires:'20m',
-        default:Date.now
-    }
+const schema = mongoose.Schema({
+  id: {
+    type: String,
+  },
+  note: {
+    type: String,
+    default: "Empty",
+  },
+  hash: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    expires: "20m",
+    default: Date.now,
+  },
 });
 
-const notes=mongoose.model("notes",schema);
-module.exports=notes;
+const notes = mongoose.model("notes", schema);
+module.exports = notes;
